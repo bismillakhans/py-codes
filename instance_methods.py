822edb6639b1b542fb232394623d66a367d1f485
bismillakhans <bismillakhans@gmail.com>
2019-12-19 01:37:38 +0530

    first commit

---
+++ instance_methods.py
@@ -0,0 +1,8 @@
+class Student():
+    def type(self):
+        print(self)
+        print(" I am a boy")
+
+
+first=Student()
+first.type()
---
+++ simple-class.py
@@ -0,0 +1,9 @@
+class Student:
+    name="Raju"
+
+stud=Student()
+print(stud)  #hexa decimal value
+print(stud.name)  #varible value
+
+
+second=Student()  #second object  
